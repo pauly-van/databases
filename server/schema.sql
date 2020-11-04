dd44420661c9d1d5be1e2f9f535de91e8853c2fc
Alek Ortiz <aleksebastiano@gmail.com>
2020-11-03 21:53:19 -0600

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  message_id integer PRIMARY KEY,
+  message_text text,
+  user_id integer
+  -- FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE users (
+  user_id integer PRIMARY KEY,
+  username text,
+  message_id integer,
+  FOREIGN KEY (message_id) REFERENCES messages(message_id)
+);
+
+CREATE TABLE rooms (
+  room_id integer PRIMARY KEY,
+  room_name text,
+  user_id integer,
+  message_id integer
+  -- FOREIGN KEY (message_id) REFERENCES messages(message_id),
+  -- FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
 /* Create other tables and define schemas for them here! */
