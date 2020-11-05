f02174d65a557c86e344eb48dd8fb6c223b1cbba
Alek Ortiz <aleksebastiano@gmail.com>
2020-11-04 22:58:20 -0600

    Still working on connecting server to db

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,18 @@
 var mysql = require('mysql');
+const db = require('../models');
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-const dbConnection = mysql.createConnection({
-  user: 'root',
-  database: 'chat'
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "chat"
 });
 
-dbConnection.connect();
-dbConnection.end();
+con.connect();
+
+module.exports.con = con;
--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,42 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (query) {
+      query = `SELECT message_text FROM messagess WHERE message_text = ${query.text}`;
+      db.con.query(query, callback => {
+        if (err) { throw err; }
+        console.log('result: ', result);
+        callback(err, result);
+      });
+    }, // a function which produces all the messages
+    post: function (query) {
+      query = `INSERT INTO messages (message_id, message_text) VALUES(1, ${query.message});`;
+      db.con.query(query, (err, result) => {
+        if (err) { throw err; }
+        console.log('result: ', result);
+      });
+    }
   },
+
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (query) {
+      query = `SELECT username FROM users WHERE username = ${query.name}`;
+      db.con.query(query, (err, result) => {
+        if (err) { throw err; }
+        console.log('result: ', result);
+        return result;
+      });
+    },
+    post: function (query) {
+      query = `INSERT INTO users (user_id, username) VALUES(1, ${query.name});`;
+      db.con.query(query, (err, result) => {
+        if (err) { throw err; }
+        console.log('result: ', result);
+        return result;
+      });
+    }
   }
 };
 
